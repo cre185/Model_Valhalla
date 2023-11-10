a89e6f140047a994af12140edc1a3296737f94b3
creeper <2907692855@qq.com>
2023-11-10 15:03:14 +0800

    image is avaliable

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import User, VerifyMsg
+from .models import User, VerifyMsg, VerifyEmail
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(VerifyMsg)
+admin.site.register(VerifyEmail)
