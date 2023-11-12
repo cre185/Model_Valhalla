76dd7fb020c533861e49cdbbdb41399f605975bb
YaoRunmao <1969623002@qq.com>
2023-11-12 19:37:19 +0800

    修改用户面板的bug

--- backend/user/views.py
+++ backend/user/views.py
@@ -56,7 +56,7 @@ class send_messageView(APIView):
         if serializer.is_valid():
             serializer.save()
             send = send_msg()
-            # send.send_sms(data["code"], data["mobile"])
+            send.send_sms(data["code"], data["mobile"])
             return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
