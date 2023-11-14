1c2e60cda4182402e0404c8ed16ba964927ecbd8
YaoRunmao <1969623002@qq.com>
2023-11-14 14:53:07 +0800

    排行榜前端组件初始化

---
+++ frontend/src/views/ranking/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home"></div>
+</template>
+
+<script lang="ts" setup>
+  import Typed from 'typed.js';
+  import { useRouter } from 'vue-router';
+
+</script>
+
+<style scoped>
+#home {
+  background: white;
+  width: 100%;
+  height: 100%;
+}
+</style>
