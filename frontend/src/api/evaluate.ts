ee44b34d899126899655a2e8b116a1f808aabfbd
YaoRunmao <1969623002@qq.com>
2023-11-24 17:19:30 +0800

    添加“对抗评测轮结果”类（初步）

--- frontend/src/api/evaluate.ts
+++ frontend/src/api/evaluate.ts
@@ -1,11 +1,14 @@
+/* eslint-disable */
 import axios from 'axios';
 import apiCat from '@/api/main';
 import { LLMListRes } from './model-list';
+import { updateComment } from "@/api/comment";
 
 export interface SelectedModel {
     id: number;
     name: string;
 }
+
 export async function queryLLMevaluateList()
 {
     const LLMList: {data: any, total: number} = { data:[], total: 0};
@@ -17,3 +20,45 @@ export async function queryLLMevaluateList()
     }
     return LLMList;
 }
+
+class QuestionAndAnswer {
+    question:string
+    answerA:string
+    answerB:string
+
+    constructor(question:string, answerA:string, answerB:string) {
+        this.question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+    }
+}
+
+class EvaluateRound {
+    modelA:number
+    modelB:number
+    user:number
+    QA:QuestionAndAnswer[]
+    result:number
+    date:string
+
+    constructor(modelA:number, modelB:number, user:number) {
+        this.modelA = modelA;
+        this.modelB = modelB;
+        this.user = user;
+        this.QA = [] as QuestionAndAnswer[];
+        this.result = -1;
+        this.date = '';
+    }
+
+    async getResponse(jwt:string) {
+        await axios.post(apiCat('/ranking/like_llm_comment'), {
+            id: this.commentId
+        }, {
+            headers: {
+                Authorization: jwt,
+            },
+        });
+    }
+}
+
+export default EvaluateRound;
