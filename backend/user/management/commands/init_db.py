5fec4c1a98f4772549fe03e48bab6e05879edf02
creeper <2907692855@qq.com>
2023-11-27 17:49:09 +0800

    optimize init_db

--- backend/user/management/commands/init_db.py
+++ backend/user/management/commands/init_db.py
@@ -12,6 +12,8 @@ class Command(BaseCommand):
         # Create the admin user
         admin = User(username="realadmin", password="realadmin", mobile=11111111111, is_admin=True)
         admin.save()
+        user = User(username="testuser", password="testuser", mobile=12345678901)
+        user.save()
         # Create the five standard datasets
         dataset1 = Dataset(name="dataset1", description="dataset1", data_file="static/data/ceval_select.csv")
         dataset1.save()
@@ -33,7 +35,10 @@ class Command(BaseCommand):
         llm4 = LLMs(name="llm4", description="llm4", model_name="zephyr_7b")
         llm4.save()
         # Create credits
+        credit_list=[31,41,25,29,29,44,22,28,37,37,37,42]
         for i in range(1,6):
             for j in range(1,5):
                 credit = Credit(dataset=Dataset.objects.get(id=i), LLM=LLMs.objects.get(id=j))
+                if i*4+j-5<len(credit_list):
+                    credit.credit=credit_list[i*4+j-5]
                 credit.save()
--- docs/source/test.md
+++ docs/source/test.md
@@ -29,9 +29,10 @@ python manage.py init_db
 ``` 
 当前的标准测试数据包括：  
 * 一个管理员账户，用户名为realadmin，密码为realadmin  
+* 一个普通用户账户，用户名为testuser，密码为testuser  
 * 5个标准的数据集，名称分别为dataset1-5，并分别对应一个题目库  
 * 4个标准的模型，名称分别为llm1-4，并分别对应一个模型的调用接口  
-* 20个这些模型/数据集对应的测试分数  
+* 20个这些模型/数据集对应的测试分数，其中客观数据集对应的测试分数已经初始化，为之前测试得到的数据  
 
 如需更改init_db命令的行为，请修改`user/management/commands/init_db.py`  
 ### 单元测试  
