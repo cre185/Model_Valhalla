88408d1b8017ee6c182445e5efc57d0e05898034
YaoRunmao <1969623002@qq.com>
2023-11-30 10:57:26 +0800

    删除后端

--- backend/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import User, VerifyEmail, VerifyMsg
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(VerifyMsg)
-admin.site.register(VerifyEmail)
