7021352aa24a032d9a1ad29618c53feb78a4cc21
likl21 <likl21@mails.tsinghua.edu.cn>
2023-12-06 21:07:10 +0800

    补充了填充问题的种类和内容，完善评价按钮和模型名字显示的样式

--- frontend/src/views/evaluate/adversarial-evaluation/index.vue
+++ frontend/src/views/evaluate/adversarial-evaluation/index.vue
@@ -72,7 +72,7 @@
                   </div>
                 </a-space>
               </div>
-              <div>
+              <div style="font-weight: bold; padding-top: 5px;">
                 {{ modelAname }}
               </div>
             </a-col>
@@ -97,7 +97,7 @@
                   </div>
                 </a-space>
               </div>
-              <div>
+              <div style="font-weight: bold; padding-top: 5px;">
                 {{ modelBname }}
               </div>
             </a-col>
@@ -341,44 +341,140 @@ const lengthRules = [
 ];
 const QuestionTypeSelectOptions = computed<SelectOptionData[]>(() => [
   {
-    label: "机器翻译",
+    label: t('evaluation.question.fill.typeone'),
     value: '机器翻译',
   },
   {
-    label: "数学运算",
+    label: t('evaluation.question.fill.typetwo'),
     value: '数学运算',
   },
+  {
+    label: t('evaluation.question.fill.typethree'),
+    value: '文本生成',
+  },
+  {
+    label: t('evaluation.question.fill.typefour'),
+    value: '推理和逻辑',
+  },
+  {
+    label: t('evaluation.question.fill.typefive'),
+    value: '语法和语义理解',
+  }
 ]);
 const QuestionSelectOptions = computed<SelectOptionData[]>(() => {
   if (formModel.value.questionType === '机器翻译') { // 之后填充的问题设定在此框架上修改具体内容即可
     return [
       {
-        label: "汉译日：大家早上好",
-        value: '请问“大家早上好”用日语怎么说',
+        label: t('evaluation.evaluate.fill.typeone.q1'),
+        value: '请将“大家早上好”翻译成日语',
       },
       {
-        label: "英译汉：I have never had such a fantastic start",
+        label: t('evaluation.evaluate.fill.typeone.q2'),
         value: '请将下面这句话翻译成中文: I have never had such a fantastic start',
       },
       {
-        label: "汉译英：今天早上你吃了什么",
-        value: '请将下面这句话翻译成英文: 今天早上你吃了什么'
+        label: t('evaluation.evaluate.fill.typeone.q3'),
+        value: '请问“今天中午你吃了什么”用英语怎么说'
+      },
+      {
+        label: t('evaluation.evaluate.fill.typeone.q4'),
+        value: 'Please translate "这本书是由一位著名的作家写的" into English'
+      },
+      {
+        label: t('evaluation.evaluate.fill.typeone.q5'),
+        value: '请将“This is a well-known restaurant located in Shanghai”翻译成日语'
       },
     ];
   }
   if (formModel.value.questionType === '数学运算') {
     return [
       {
-        label: "114+514等于多少",
+        label: t('evaluation.evaluate.fill.typetwo.q1'),
         value: '114+514等于多少',
       },
       {
-        label: "sin(x)的导数是多少",
-        value: 'sin(x)的导数是多少',
+        label: t('evaluation.evaluate.fill.typetwo.q2'),
+        value: 'sin(x)关于x的导数是多少',
       },
       {
-        label: "7整除36，余数是多少",
-        value: '7整除36，余数是多少',
+        label: t('evaluation.evaluate.fill.typetwo.q3'),
+        value: '36除以7，余数是多少',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typetwo.q4'),
+        value: '计算下面的式子："2 * (3 + 5) / 4"',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typetwo.q5'),
+        value: '已知2x+4=10，x的值是多少',
+      },
+    ];
+  }
+  if (formModel.value.questionType === '文本生成') {
+    return [
+      {
+        label: t('evaluation.evaluate.fill.typethree.q1'),
+        value: '简述一下电影《战狼II》的情节',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typethree.q2'),
+        value: '讲一个冷笑话',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typethree.q3'),
+        value: '请写一首关于自由的诗歌',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typethree.q4'),
+        value: '我计划去得克萨斯州旅行，请给我一份旅行指南',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typethree.q5'),
+        value: '我最近想开始健身，请给我一些关于饮食上的建议',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typethree.q6'),
+        value: '评价一下《原神》这款游戏',
+      },
+    ];
+  }
+  if (formModel.value.questionType === '推理和逻辑') {
+    return [
+      {
+        label: t('evaluation.evaluate.fill.typefour.q1'),
+        value: '如果今天是星期五，明天是哪一天？',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typefour.q2'),
+        value: '假设A是B的兄弟，而B是C的兄弟，是否可以得出A是C的兄弟的结论？',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typefour.q3'),
+        value: '如果所有人都会死亡，那么小明是否会死？',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typefour.q4'),
+        value: '汤姆有一只喜欢骨头的宠物，那么他的宠物可能是狗吗？',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typefour.q5'),
+        value: '小刚的父母都是AB型血，小刚可能是O型血吗',
+      },
+    ];
+  }
+  if (formModel.value.questionType === '语法和语义理解') {
+    return [
+      {
+        label: t('evaluation.evaluate.fill.typefive.q1'),
+        value: '找出下列句子中的语法错误："Every one of the students are here"',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typefive.q2'),
+        value: '找出下列句子中的语义错误：自行车早上喜欢骑小明',
+      },
+      {
+        label: t('evaluation.evaluate.fill.typefive.q3'),
+        value: '检查下列句子是否有语法错误："He don\'t like apples."',
       },
     ];
   }
@@ -404,6 +500,9 @@ const selectClick = () => {
   selectVisible.value = true;
 };
 const evaluateClick = async () => {
+  if (sendQuestionsDisabled.value === true) {
+    return;
+  }
   if (!formModel.value.question || formModel.value.question.trim() === '')
   {
     window.alert(proxy.$t('evaluation.question.button.emptyMsg'));
@@ -458,7 +557,8 @@ const aBetterClick = async () => { // 前面的getmodelB调用后没有及时更
     const element = document.getElementById('aBetter');
 
     if (element) {
-      element.style.backgroundColor = 'dodgerblue';
+      element.style.backgroundColor = 'rgb(45, 92, 246)';
+      element.style.color = 'white';
     }
     await round.updateEloResult();
   }
@@ -477,7 +577,8 @@ const bBetterClick = async () => {
     const element = document.getElementById('bBetter');
 
     if (element) {
-      element.style.backgroundColor = 'dodgerblue';
+      element.style.backgroundColor = 'rgb(45, 92, 246)';
+      element.style.color = 'white';
     }
     await round.updateEloResult();
   }
@@ -496,7 +597,8 @@ const abGoodClick = async () => {
     const element = document.getElementById('abGood');
 
     if (element) {
-      element.style.backgroundColor = 'dodgerblue';
+      element.style.backgroundColor = 'rgb(45, 92, 246)';
+      element.style.color = 'white';
     }
     await round.updateEloResult();
   }
@@ -514,7 +616,8 @@ const abBadClick = async () => {
     const element = document.getElementById('abBad');
 
     if (element) {
-      element.style.backgroundColor = 'dodgerblue';
+      element.style.backgroundColor = 'rgb(45, 92, 246)';
+      element.style.color = 'white';
     }
     await round.updateEloResult();
   }
