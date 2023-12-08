82f650f1a98e51ab1d4880adf8ea1639f0a30f64
likl21 <likl21@mails.tsinghua.edu.cn>
2023-12-09 01:02:54 +0800

    初步搭建上传框架

---
+++ frontend/src/views/dataset/components/datasetupload.vue
@@ -0,0 +1,45 @@
+<template>
+    <a-modal
+        :title="null"
+        :closable=false>
+        <template #title>
+            <div style="display: flex; align-items: center;">
+                <icon-upload style="margin-right: 5px;"></icon-upload>
+                {{ $t('dataset.upload.title') }}
+            </div>
+        </template>
+        <a-form style="padding-right: 10px;">
+            <a-form-item
+                :label="$t('dataset.upload.introduction.title')">
+                <a-textarea
+                    :placeholder="$t('dataset.upload.introduction.title.default')"
+                    style="height: 100px;">
+
+                </a-textarea>
+            </a-form-item>
+            <a-form-item
+                :label="$t('dataset.upload.publisher.title')">
+                <a-input
+                    :placeholder="$t('dataset.upload.publisher.title.default')">
+
+                </a-input>
+            </a-form-item>
+            <a-form-item
+                :label="$t('dataset.upload.application.title')">
+                <a-input
+                    :placeholder="$t('dataset.upload.application.title.default')">
+
+                </a-input>
+            </a-form-item>
+        </a-form>
+    </a-modal>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
+<style scoped lang="less">
+
+</style>
--- frontend/src/views/dataset/index.vue
+++ frontend/src/views/dataset/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="container">
     <Breadcrumb :items="['menu.dataset', 'menu.dataset.details']" />
+    <a-button
+    type="primary"
+    @click="UploadClick"
+    >Show Dataset Upload</a-button>
+    <DatasetUpload
+    v-model:visible="showDatasetUpload"/>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {ref} from 'vue';
+import DatasetUpload from "./components/datasetupload.vue";
 
+const showDatasetUpload = ref(false);
+const UploadClick = () => {
+  showDatasetUpload.value = true;
+}
 </script>
 
 <style scoped lang="less">
