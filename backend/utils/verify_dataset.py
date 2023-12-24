d7b384199677bf3c8b0cfb5d5ff83cc39e42ee99
creeper <2907692855@qq.com>
2023-12-24 20:46:35 +0800

    update preview

--- backend/utils/verify_dataset.py
+++ backend/utils/verify_dataset.py
@@ -1,5 +1,6 @@
 import csv
 from io import StringIO
+import json
 
 def verify_dataset(content):
     if not content:
@@ -30,10 +31,47 @@ def verify_dataset(content):
             raise Exception("Invalid file")
     return subjective, line_count
 
+def get_first_10_rows(content):
+    content = StringIO(content)
+    reader = csv.reader(content)
+
+    header_list = next(reader)
+    for i in range(len(header_list)):
+        header_list[i] = header_list[i].lower()
+    headers = set(header_list)
+    obj1 = set(['question', 'choices', 'answer'])
+    obj2 = set(['question', 'a','b','c','d', 'answer'])
+    sub1 = set(['prompt'])
+    if obj1.issubset(headers):
+        filters = ['question', 'choices', 'answer']
+    elif obj2.issubset(headers):
+        filters = ['question', 'a','b','c','d', 'answer']
+    elif sub1.issubset(headers):
+        filters = ['prompt']
+    else:
+        return [], False
+
+    data = []
+    for line in reader:
+        if line == '':
+            continue
+        data.append([])
+        for i in range(len(line)):
+            if header_list[i] in filters:
+                if header_list[i] == 'choices':
+                    choices = json.loads(line[i].replace('""', '"'))
+                    for choice in choices:
+                        data[-1].append(choice)
+                else:
+                    data[-1].append(line[i])
+        if len(data) == 10:
+            break
+    return data, filters==['prompt']
+
 if __name__ == '__main__':
-    with open('mmlu_select.csv', 'r', encoding='utf-8') as f:
+    with open('zbench_common.csv', 'r', encoding='utf-8') as f:
         try:
             content = f.read()
-            print(verify_dataset(content))
+            print(get_first_10_rows(content))
         except Exception as e:
             print(e)
