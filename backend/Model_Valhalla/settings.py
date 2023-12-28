2818228ad6b7294b2be452bd30362162d56535ab
creeper <2907692855@qq.com>
2023-12-28 19:56:07 +0800

    fix more bugs in deployment

--- backend/Model_Valhalla/settings.py
+++ backend/Model_Valhalla/settings.py
@@ -32,7 +32,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -51,6 +50,8 @@ INSTALLED_APPS = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
